3b1b801081315a508e1cf1460e7e88b2f8a04c75
Kalenga243 <kevinkalenga10@gmail.com>
2025-07-24 00:04:11 +0200

    Message and Loader Component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,21 +5,6 @@ import {
     PRODUCT_LIST_FAIL
 } from '../constants/productConstants'
 
-// export const listProducts = async (dispatch) => {
-//    try {
-//       dispatch({type: PRODUCT_LIST_REQUEST})
-//       const { data } = await axios.get('/api/products/');
-//       dispatch({type: PRODUCT_LIST_SUCCESS, payload: data})
-//    } catch (error) {
-//       dispatch({
-//         type: PRODUCT_LIST_FAIL,
-//         payload: error.response && error.response.data.message
-//         ? error.response.data.message
-//         : error.message,
-//       })
-//    }
-// }
-
 export const listProducts = () => async (dispatch) => {
    try {
       dispatch({ type: PRODUCT_LIST_REQUEST });
