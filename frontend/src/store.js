02a9a1952a650fcd169e38a71b7118a6a13c8fae
Kalenga243 <kevinkalenga10@gmail.com>
2025-07-23 02:28:21 +0200

    Product List Reducer and Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { productListReducer } from './reducers/ProductReducers';
+
+const reducer = combineReducers({
+    productList: productListReducer
+});
 
 const store = configureStore({
-    reducer: {}
+    reducer: reducer, // ← c’est ici que tu dois utiliser ton combineReducers
+    // middleware, devTools, etc. peuvent aussi être ajoutés ici si nécessaire
 });
 
 export default store;
